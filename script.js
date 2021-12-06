3c1a3a33c0ab7261a5a36a1968a5c3f484798dfe
Sebastian Hönig <hoenigsebastian@gmail.com>
2021-12-06 18:09:19 +0100

    added ability to set start/end node, added ability to clear grid

--- script.js
+++ script.js
@@ -4,8 +4,29 @@ let cells = document.getElementsByClassName("cell");
 
 const NO_COLS = 50;
 const NO_ROWS = 20;
-const START_NODE = 566;
-const END_NODE = 590;
+let start_node_exists = false;
+let end_node_exists = false;
+
+//somehow classes?
+//if onclick doesnt work, do buttons with text field
+
+function setNode(div) {
+  if (start_node_exists) {
+    setEndNode(div);
+    end_node_exists = true;
+  } else {
+    setStartNode(div);
+    start_node_exists = true;
+  }
+}
+
+function setStartNode(div) {
+  div.classList.add("start");
+}
+
+function setEndNode(div) {
+  div.classList.add("end");
+}
 
 function createGrid(NO_ROWS, NO_COLS) {
   for (i = 0; i < NO_ROWS; i++) {
@@ -23,29 +44,32 @@ function createGrid(NO_ROWS, NO_COLS) {
   }
 }
 
-function colourStartAndEnd(START_NODE, END_NODE) {
+/* function colourStartAndEnd(START_NODE, END_NODE) {
   cells[START_NODE].classList.add("start");
   cells[END_NODE].classList.add("end");
-}
+} */
 
-function doBfs(START_NODE, END_NODE) {
-  if (!START_NODE || !END_NODE || START_NODE === END_NODE) {
+async function doBfs(start_node, end_node) {
+  if (start_node === end_node) {
     return;
   }
-  let end_node = cells[END_NODE];
-  let start_node = cells[START_NODE];
   let queque = [];
   queque.push(start_node);
   start_node.setAttribute("visited", "true");
+  start_node.classList.add("visited");
   while (queque) {
     let current_node = queque.shift();
     let neighbours = findNeighbours(current_node);
     for (let neighbour of neighbours) {
+      neighbour.setAttribute("visited", "true");
+      neighbour.classList.add("current");
+      await sleep(200);
+      neighbour.classList.remove("current");
+      neighbour.classList.add("visited");
       if (neighbour === end_node) {
-        console.log("Shortest path found");
+        visualizePath(end_node, start_node);
         return neighbour;
       } else {
-        neighbour.setAttribute("visited", "true");
         queque.push(neighbour);
       }
     }
@@ -64,7 +88,9 @@ function findNeighbours(current_node) {
         CSS.escape(y) +
         "]"
     );
-    if (left_neighbour.getAttribute("visited") === "false") {
+    if (left_neighbour.getAttribute("visited") == "false") {
+      left_neighbour.setAttribute("previous_x", x);
+      left_neighbour.setAttribute("previous_y", y);
       neighbour_array.push(left_neighbour);
     }
   }
@@ -76,7 +102,9 @@ function findNeighbours(current_node) {
         CSS.escape(y) +
         "]"
     );
-    if (right_neighbour.getAttribute("visited") === "false") {
+    if (right_neighbour.getAttribute("visited") == "false") {
+      right_neighbour.setAttribute("previous_x", x);
+      right_neighbour.setAttribute("previous_y", y);
       neighbour_array.push(right_neighbour);
     }
   }
@@ -88,8 +116,9 @@ function findNeighbours(current_node) {
         CSS.escape((parseInt(y) - 1).toString()) +
         "]"
     );
-    neighbour_array.push(top_neighbour);
-    if (top_neighbour.getAttribute("visited") === "false") {
+    if (top_neighbour.getAttribute("visited") == "false") {
+      top_neighbour.setAttribute("previous_x", x);
+      top_neighbour.setAttribute("previous_y", y);
       neighbour_array.push(top_neighbour);
     }
   }
@@ -101,12 +130,79 @@ function findNeighbours(current_node) {
         CSS.escape((parseInt(y) + 1).toString()) +
         "]"
     );
-    if (bottom_neighbour.getAttribute("visited") === "false") {
+    if (bottom_neighbour.getAttribute("visited") == "false") {
+      bottom_neighbour.setAttribute("previous_x", x);
+      bottom_neighbour.setAttribute("previous_y", y);
       neighbour_array.push(bottom_neighbour);
     }
   }
   return neighbour_array;
 }
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function visualizePath(end_node, start_node) {
+  let path = [];
+  path.push(end_node);
+  let curr_node = end_node;
+  while (curr_node != start_node) {
+    let previous_node = document.querySelector(
+      "[x=" +
+        CSS.escape(curr_node.getAttribute("previous_x")) +
+        "][y=" +
+        CSS.escape(curr_node.getAttribute("previous_y")) +
+        "]"
+    );
+    path.push(previous_node);
+    curr_node = previous_node;
+  }
+  path.reverse();
+  for (let node of path) {
+    await sleep(30);
+    node.classList.add("path");
+    node.classList.remove("visited");
+  }
+}
+
 createGrid(NO_ROWS, NO_COLS);
-colourStartAndEnd(START_NODE, END_NODE);
+/* colourStartAndEnd(START_NODE, END_NODE); */
+
+function startVisualizer() {
+  if (start_node_exists && end_node_exists) {
+    let start_node = document.getElementsByClassName("start")[0];
+    let end_node = document.getElementsByClassName("end")[0];
+    doBfs(start_node, end_node);
+  }
+}
+
+function clearGrid() {
+  for (let cell of cells) {
+    cell.classList.remove("path");
+    cell.classList.remove("visited");
+    cell.setAttribute("visited", "false");
+    cell.removeAttribute("previous_x");
+    cell.removeAttribute("previous_y");
+    cell.classList.remove("start");
+    cell.classList.remove("end");
+    start_node_exists = false;
+    end_node_exists = false;
+  }
+}
+
+for (j = 0; j < cells.length; j++) {
+  cells[j].addEventListener("click", function () {
+    if (start_node_exists) {
+      if (end_node_exists) {
+        alert("Exists!");
+      } else {
+        setEndNode(this);
+        end_node_exists = true;
+      }
+    } else {
+      setStartNode(this);
+      start_node_exists = true;
+    }
+  });
+}
